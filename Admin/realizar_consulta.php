6c38535971664e7225f1db140f33ce9d00042fe5
Edward Brito <Edwardbrito11@hotmail.com>
2021-07-15 13:35:24 -0400

    formulario para update y insert

---
+++ Admin/realizar_consulta.php
@@ -0,0 +1,157 @@
+<?php
+if (!empty($_GET['id'])) {
+    include_once('../php/conexionDB.php');
+    include_once('../php/consultas.php');
+    $id = $_GET['id'];
+    $row = ConsultarCitas($link, $id); //resultado es igual a lo que me devuelva esa funcion
+    $resultado = MostrarCitas($link);
+} else {
+}
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- ICONO -->
+    <link rel="icon" href="../src/img/logo.png" type="image/png" />
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../src/css/lib/bootstrap/css/bootstrap.min.css">
+
+    <!-- Style -->
+    <link rel="stylesheet" href="../src/css/admin.css">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../src/css/lib/fontawesome/css/all.css">
+
+    <!-- Datatable libreria -->
+    <link rel="stylesheet" href="../src/js/lib/datatable\css\jquery.dataTables.min.css">
+    <link rel="stylesheet" href="../src/js/lib/datatable\css\responsive.dataTables.min.css">
+
+    <title>Perfect Teeth </title>
+</head>
+
+<body>
+
+
+    <aside class="sidebar">
+        <div class="toggle">
+            <a href="#" class="burger js-menu-toggle" data-toggle="collapse" data-target="#main-navbar">
+                <span></span>
+            </a>
+        </div>
+        <div class="side-inner">
+
+            <div class="profile">
+                <img src="../src/img/admin_user.png" alt="Image" class="img-fluid">
+                <h3 class="name"><?php $row['nombre'] . ' ' . $row['apellido']; ?></h3>
+                <span class="country">Perfect Teeth </span>
+            </div>
+
+
+            <div class="nav-menu">
+                <ul>
+                    <li class="accordion">
+
+                    <li><a href="inicioAdmin.php"><span class="icon-location-arrow mr-3"></span> <i class="far fa-calendar-check"></i>
+                            Citas
+                            pendientes
+                        </a></li>
+                    <li><a href="doctores.php"><span class="icon-location-arrow mr-3"></span><i class="fas fa-user-md"></i>
+                            Dentistas</a></li>
+                    <li><a href="calendar.php"><span class="icon-pie-chart mr-3"></span> <i class="far fa-calendar-alt"></i>
+                            Calendario</a>
+                    </li>
+                    <li><a href="../php/cerrar.php"><span class="icon-sign-out mr-3"></span><i class="fas fa-sign-out-alt"></i> Cerrar
+                            sesión </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </aside>
+    <main class="bg bg-white">
+        <div class="site-section ">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-12">
+                        <div class="content-box-large">
+                            <div>
+                                <ol class=" breadcrumb bg-white">
+                                    <li class="breadcrumb-item active">
+                                        Inicio
+                                    </li>
+                                </ol>
+                            </div>
+                            <div class="panel-body">
+                                <div class="row">
+
+                                    <div class="container">
+                                        <div class="p-3 mb-2 bg-primary text-white text-center">Realizar diagnóstico sobre la cita</div>
+
+                                        <form action="../crud/realizar_consultasUPDATE.php?accion=UDT" method="POST" enctype="multipart/form-data" autocomplete="off" class="form-horizontal">
+
+                                            <input type="hidden" name="id" id="id" value="<?php echo $row['id_cita'] ?>">
+                                            <!-- para almacenar los id que el usuario logea -->
+
+                                            <div class="card">
+
+                                                <div class="card-body">
+                                                    <h5 class="card-title">Diagnóstico </h5>
+                                                    <p class="card-text"></p>
+
+                                                </div>
+                                            </div>
+                                            <div class=" form-group">
+                                                <div class="row">
+                                                    <!-- primera columna -->
+                                                    <div class=" col-md-4">
+                                                        <label for="apellidos">Descripción </label>
+                                                        <input class="form-control" type="text" name="Descripción" placeholder="Descripción" required>
+                                                    </div>
+
+                                                    <!-- segunda columna -->
+                                                    <div class=" col-md-4">
+                                                        <label for="apellidos">Medicina </label>
+                                                        <input class="form-control" type="text" name="Medicina" placeholder="Medicina opcional" required>
+                                                    </div>
+
+
+
+                                                </div>
+
+                                                <div class="ol-md-4" style="margin-top: 10%;">
+                                                    <button class="btn btn-success btn-lg " type="submit" name="guardar" value="Guardar">
+                                                        <i class="far fa-save"></i> Guardar
+                                                    </button>
+                                                </div>
+                                                <div class="form-group">
+                                                    <a href="./inicioAdmin.php"> <i class="fas fa-history"></i> Atrás </a>
+                                                </div>
+
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+
+
+    </main>
+
+
+    <script src="../src/js/jquery.js"></script>
+    <!-- <script src="../src/css/lib/bootstrap/js/bootstrap.min.js"></script> -->
+    <script src="../src/js/admin.js"></script>
+
+</body>
+
+</html>
---
+++ crud/realizar_consultasUPDATE.php
@@ -0,0 +1,59 @@
+<?php
+try {
+    //code...
+    include_once('../php/conexionDB.php');
+    include_once('../php/consultas.php');
+    if (!empty($_GET['accion'])) {
+        # code...
+        $opcion = $_GET['accion'];
+    } else {
+        session_start();
+        $_SESSION['MensajeTexto'] = "Avertencia: Accion realilzada no permitada";
+        $_SESSION['MensajeTipo'] = "is-warning";
+        header("Location: ../admin/inicioAdmin.php");
+    }
+
+    //CRUD - INS - DLT -- UDT
+    switch ($opcion) {
+        case 'UDT':
+            //$estado = filter_var($_POST['estado'], FILTER_SANITIZE_NUMBER_INT);  //para filtrar la data a entero
+            $id = filter_var($_POST['id'], FILTER_SANITIZE_NUMBER_INT);
+            $descripcion = filter_var($_POST['Descripción'], FILTER_SANITIZE_STRING);  //para filtrar la data
+            $medicina = filter_var($_POST['Medicina'], FILTER_SANITIZE_STRING);  //para filtrar la data
+
+            $query1 = " 
+            INSERT INTO `paciente_diagnostico`(`id_cita`, `descripcion`, `medicina`) VALUES ('$id', '$descripcion',  '$medicina')";
+            $query = "UPDATE `citas` SET `estado` = 'A'   WHERE `id_cita` = '$id'"; //para actualizar
+
+
+            $resultado = mysqli_query($link, $query) && $resultado1 = mysqli_query($link, $query1); //Si devuelve True se ejecuto con exito y si no pues no
+            if (!$resultado) {
+                $_SESSION['MensajeTexto'] = "Error actualizando el registro ";
+                $_SESSION['MensajeTipo'] = "p-3 mb-2 bg-danger text-white";
+                header("Location: ../admin/inicioAdmin.php ");
+                // die("Error en base de datos: " . mysqli_error($link));
+            } else {
+                $_SESSION['MensajeTexto'] = "Registro actualizado con exito";
+                $_SESSION['MensajeTipo'] = "p-3 mb-2 bg-info text-white";
+                header("Location: ../admin/inicioAdmin.php");
+                //die("Error en base de datos: " . mysqli_error($link));
+            }
+            //cerrando conexion
+            mysqli_close($link);
+            break;
+
+        default:
+            # code...
+            $_SESSION['MensajeTexto'] = "Avertencia: No se pudo identificar la accion a realizar";
+            $_SESSION['MensajeTipo'] = "is-warning";
+            header("Location: ../admin/inicioAdmin.php");
+            //die("Error en base de datos: ". mysqli_error($link));  //muestra en pantalla el error que se ejecuta
+            break;
+    }
+} catch (Exception $e) {
+    print "Exception no controlado 01" . $e->getMessage();
+    print "Estamos trabajando en corregir esta situacion";
+} catch (Error $e) {
+    print "Error no controlado 01" . $e->getMessage();
+    print "Estamos trabajando en corregir esta situacion";
+}
