7cb2e31e6b07502d02e88c367dc19ce87df55307
Edward Brito <Edwardbrito11@hotmail.com>
2021-07-19 19:48:21 -0400

    update

--- Admin/calendar.php
+++ Admin/calendar.php
@@ -31,64 +31,78 @@ if (isset($_SESSION['id_doctor'])) {
 
 
 	<!-- jQuery UI -->
-	<link href="https://code.jquery.com/ui/1.10.3/themes/redmond/jquery-ui.css" rel="stylesheet" media="screen">
+	<!-- <link rel="shortcut icon" href="https://www.jose-aguilar.com/blog/wp-content/themes/jaconsulting/favicon.ico" /> -->
+	<!-- <link rel="stylesheet" href="css/font-awesome.min.css"> -->
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<!-- <link rel="stylesheet" href="css/styles.css"> -->
+	<link rel='stylesheet' type='text/css' href='css/fullcalendar.css' />
 	<script src="https://code.jquery.com/jquery-3.2.1.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-
-	<link href='../src/js/lib/fullcalendar/main.css' rel='stylesheet' />
-	<script src='../src/js/lib/fullcalendar/main.js'></script>
-	<script src='../src/js/lib/fullcalendar/locale/moment.min.js'></script>
-	<script type='text/javascript' src='../src/js/lib/fullcalendar/locale/es.js'></script>
-
-
+	<script type='text/javascript' src='../src/js/lib/FullCalendar/moment.min.js'></script>
+	<script type='text/javascript' src='../src/js/lib/FullCalendar/fullcalendar.min.js'></script>
+	<script type='text/javascript' src='../src/js/lib/FullCalendar/locale/es.js'></script>
 	<script>
-		document.addEventListener('DOMContentLoaded', function() {
-			var calendarEl = document.getElementById('calendar');
+		function addZero(i) {
+			if (i < 10) {
+				i = '0' + i;
+			}
+			return i;
+		}
+		var hoy = new Date();
+		var dd = hoy.getDate();
+		if (dd < 10) {
+			dd = '0' + dd;
+		}
 
-			var calendar = new FullCalendar.Calendar(calendarEl, {
-				locale: 'es',
-				initialView: 'dayGridMonth',
-				initialDate: '2021-07-22',
-				headerToolbar: {
-					left: 'prev,next today',
+		if (mm < 10) {
+			mm = '0' + mm;
+		}
+		var mm = hoy.getMonth() + 1;
+		var yyyy = hoy.getFullYear();
+
+		dd = addZero(dd);
+		mm = addZero(mm);
+
+		$(document).ready(function() {
+			$('#calendar').fullCalendar({
+				header: {
+					left: 'prev,next',
 					center: 'title',
-					right: 'dayGridMonth,timeGridWeek,timeGridDay'
+					right: 'month,agendaWeek,agendaDay'
 				},
-
-
-				eventRender: function(info) {
-					var tooltip = new Tooltip(info.el, {
-						title: info.event.extendedProps.description,
-						placement: 'top',
-						trigger: 'hover',
-						container: 'body'
-					});
-				},
-
+				defaultDate: yyyy + '-' + mm + '-' + dd,
+				buttonIcons: true, // show the prev/next text
+				weekNumbers: false,
+				editable: true,
+				eventLimit: true, // allow "more" link when too many events 
 				events: [
 					<?php
 					while ($row1 = mysqli_fetch_array($resultadoDentistas)) {
 					?> {
 							id: '<?php echo $row1['id_cita']  ?>',
 							title: '<?php echo $row1['tipo']  ?>',
-							description: 'ahahdjj',
+							description: '<?php echo  'El paciente ' . $row1['nombre'] . ' ha realizado una consulta de ' . $row1['tipo'] . '  con el doctor ' . $row1['nombreD'] . '.' . '<br>' . 'Fecha de la cita: ' . $row1['fecha_cita']  . '<br>' . 'Hora de la cita: ' . $row1['hora_cita']    ?>',
 							start: '<?php echo $row1['fecha_cita']; ?>',
 							textColor: 'White',
-							display: 'block'
+							display: 'backgr'
 
 						},
 					<?php
 					}
 					?>
-				]
+				],
+
+				eventClick: function(calEvent, jsEvent, view) {
+					$('#event-title').text(calEvent.title);
+					$('#event-description').html(calEvent.description);
+					$('#modal-event').modal();
+				},
 			});
-			calendar.render();
 		});
 	</script>
 	<title>Perfect Teeth </title>
-
 
 </head>
 
@@ -147,33 +161,40 @@ if (isset($_SESSION['id_doctor'])) {
 								</div>
 
 								<div class="panel-body">
-
 									<div class="row">
 										<div class="col-md-12 text-info">
 											<div class="p-3 mb-2 bg-info text-white text-center"> Calendario de Citas
 											</div>
 											<div class="row">
 												<div id="content" class="col-lg-12">
-													<div id="calendar"></div>
-													<div id="demo"></div>
+													<!-- <div id="calendar"></div> -->
 
-													<!-- Modal content-->
-													<div class="modal-content">
-														<div class="modal-header">
-															<button type="button" class="close" data-dismiss="modal">&times;</button>
-															<h4 class="modal-title">Cronograma de Actividade</h4>
-														</div>
+													<div class="row">
+														<div id="content" class="col-lg-12">
+															<div id="calendar"></div>
+															<div class="modal fade" id="modal-event" tabindex="-1" role="dialog" aria-labelledby="modal-eventLabel" aria-hidden="false">
+																<div class="modal-dialog" role="document">
+																	<div class="modal-content">
+																		<div class="modal-header">
+																			<h5 class="modal-title text-primary 	" id="event-title"></h5>
+																			<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+																				<span aria-hidden="true">&times;</span>
+																			</button>
+																		</div>
+																		<div class="modal-body text-dark">
+																			<div id="event-description"></div>
+																		</div>
+																		<div class="modal-footer">
+																			<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
 
-														<div class="modal-body">
-															<div class="doctor-detail-wrap">
-																<div id="doctor-calendar"></div>
+																		</div>
+																	</div>
+																</div>
 															</div>
 														</div>
-														<div class="modal-footer">
-															<!-- <input type="submit" class="btn btn-warning" id="doc-update" value="Update"> -->
-															<button type="button" class="btn btn-default" id="plist-close" data-dismiss="modal">Close</button>
-														</div>
 													</div>
+
+
 
 												</div>
 											</div>
@@ -186,6 +207,7 @@ if (isset($_SESSION['id_doctor'])) {
 						</div>
 					</div>
 				</div>
+			</div>
 			</div>
 
 		</main>
