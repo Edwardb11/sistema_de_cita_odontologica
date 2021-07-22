64bde6b0dc503bc91ae816e69e2f3fde1e8616e2
Edward Brito <Edwardbrito11@hotmail.com>
2021-07-22 16:55:20 -0400

    update

--- Reportes/reporte.php
+++ Reportes/reporte.php
@@ -6,7 +6,7 @@ include_once('../php/consultas.php');
 $Usuario = $_SESSION['nombre'];
 if (isset($_SESSION['id_paciente'])) {
     $vUsuario = $_SESSION['id_paciente'];
-    $row = consultarPaciente($link, $vUsuario);
+    $row1 = consultarPaciente($link, $vUsuario);
 } else {
     $_SESSION['MensajeTexto'] = "Error acceso al sistema  no registrado.";
     $_SESSION['MensajeTipo'] = "p-3 mb-2 bg-danger text-white";
@@ -24,14 +24,37 @@ class PDF extends FPDF
         $this->SetFont('Arial', 'B', 16);
         $this->Cell(60);
         $this->Cell(70, 10, 'HISTORIAL MEDICO', 0, 0, 'C');
+        $this->Cell(70, 20, 'Paciente' . ' ' .   $row1['nombre'], 0, 0, 'C');
         $this->Ln(50);
 
         $this->SetFont('Arial', 'B', 14);
-        $this->Cell(40, 10, 'Paciente', 1, 0, 'C', 0);
-        $this->Cell(60, 10, 'Consultas Pendientes', 1, 0, 'C', 0);
-        $this->Cell(30, 10, 'Fecha', 1, 0, 'C', 0);
-        $this->Cell(30, 10, 'Hora', 1, 0, 'C', 0);
-        $this->Cell(30, 10, 'Doctor', 1, 1, 'C', 0);
+        $this->SetFillColor(9, 199, 189); //Gris tenue de cada fila
+        $this->SetTextColor(3, 3, 3); //Color del texto: Negro
+        $bandera = true; //Para alternar el relleno
+        $this->Cell(40, 10, 'Paciente', 1, 0, 'C', $bandera);
+        $this->Cell(60, 10, 'Consultas Pendientes', 1, 0, 'C', $bandera);
+        $this->Cell(30, 10, 'Fecha', 1, 0, 'C', $bandera);
+        $this->Cell(30, 10, 'Hora', 1, 0, 'C', $bandera);
+        $this->Cell(30, 10, 'Doctor', 1, 1, 'C', $bandera);
+    }
+    function header2()
+    {
+
+        $this->Image('../src/img/logo.png', 90, 16, 33);
+        $this->SetFont('Arial', 'B', 16);
+        $this->Cell(60);
+        $this->Cell(70, 10, 'HISTORIAL MEDICO', 0, 0, 'C');
+        $this->Ln(50);
+
+        $this->SetFont('Arial', 'B', 14);
+        $this->SetFillColor(9, 199, 189); //Gris tenue de cada fila
+        $this->SetTextColor(3, 3, 3); //Color del texto: Negro
+        $bandera = true; //Para alternar el relleno
+        $this->Cell(40, 10, 'Paciente', 1, 0, 'C', $bandera);
+        $this->Cell(60, 10, 'Consultas Realizadas', 1, 0, 'C', $bandera);
+        $this->Cell(30, 10, 'Fecha', 1, 0, 'C', $bandera);
+        $this->Cell(30, 10, 'Hora', 1, 0, 'C', $bandera);
+        $this->Cell(30, 10, 'Doctor', 1, 1, 'C', $bandera);
     }
 
 
@@ -49,6 +72,21 @@ class PDF extends FPDF
 
 
 $resultado = CitasPendientesFPDF($link, $vUsuario);
+//$pdf->Ln(50);
+$pdf = new PDF();
+$pdf->AliasNbPages();
+$pdf->AddPage();
+$pdf->SetFont('Arial', '', 10);
+
+while ($row = $resultado->fetch_assoc()) {
+    $pdf->Cell(40, 10, utf8_decode($row['nombre']), 1, 0, 'C', 0);
+    $pdf->Cell(60, 10, utf8_decode($row['tipo']), 1, 0, 'C', 0);
+
+    $pdf->Cell(30, 10, $row['fecha_cita'], 1, 0, 'C', 0);
+    $pdf->Cell(30, 10, $row['hora_cita'], 1, 0, 'C', 0);
+    $pdf->Cell(30, 10, $row['nombreD'], 1, 1, 'C', 0);
+}
+$pdf->Output();
 
 $pdf = new PDF();
 $pdf->AliasNbPages();
