b71a49f4af30ad638da57047a49ec0205cd6abe8
Edward Brito <Edwardbrito11@hotmail.com>
2021-07-29 12:16:54 -0400

    datepicker scritp

--- src/js/Datepicker.js
+++ src/js/Datepicker.js
@@ -5,10 +5,12 @@ $(function () {
 $(function () {
     $('#fecha_cita').datepicker({
         beforeShowDay: $.datepicker.noWeekends,
-        minDate: 0
+        minDate: 0,
+        dateFormat: 'yy-mm-dd'
     });
 
 });
+
 
 
 
